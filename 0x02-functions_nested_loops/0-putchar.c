22b2c40ea70ec2ccec9be56a8559dd7eebd2a1c3
Preshcode <preciousokeke342@gmail.com>
2022-03-15 10:22:44 -0700

    put

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
 #include "main.h"
-#include "main.c"
 
 /**
- *main - Putchar
+ *main - prints _putchar
  *
- *_putchar print the characters given
- *Return: Always 0 (Success)
+ *Description: prints _putchar
+ *
+ *Return: 0 (Success)
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
 	_putchar('\n');
+
 	return (0);
 }
