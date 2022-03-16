c090d0e0e0e8ebaaa2b2478ef09cecc9e7684251
Preshcode <preciousokeke342@gmail.com>
2022-03-16 12:33:17 -0700

    git

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,7 @@ void times_table(void)
 				if (j != 0)
 					_putchar(' ');
 				_putchar(n + '0');
-				
+
 				if (j == 9)
 					continue;
 				_putchar(',');
@@ -32,7 +32,7 @@ void times_table(void)
 			{
 				_putchar((n / 10) + '0');
 				_putchar((n % 10) + '0');
-				if (j ==9)
+				if (j == 9)
 					continue;
 				_putchar(',');
 				_putchar(' ');
